f5047b01832161dacf8d8e5a8c95989b94e538d1
Ferengi2002 <juan.rengifo@epn.edu.ec>
2024-08-15 08:42:45 -0500

    ruleta

--- fisicaventura/src/components/Roulette.js
+++ fisicaventura/src/components/Roulette.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useCallback } from 'react';
 import ruletaImage from '../assets/images/ruleta.png';
 import flechaImage from '../assets/images/flecha.png';
 import closeImage from '../assets/images/close.png';
@@ -13,7 +13,7 @@ const Roulette = ({ onClose }) => {
     if (!isSpinning) {
       girar();
     }
-  }
+  };
 
   const girar = () => {
     setIsSpinning(true);
@@ -21,17 +21,17 @@ const Roulette = ({ onClose }) => {
     setShowResult(false);
     const randomRotation = Math.floor(Math.random() * 1440) + 720;
     setRotation(prevRotation => prevRotation + randomRotation);
-    
+
     setTimeout(() => {
       setIsSpinning(false);
       setShowResult(true);
     }, 6000);
-  }
+  };
 
-  const final = () => {
+  const final = useCallback(() => {
     const grados = (rotation % 360 + 360) % 360;
     let casillas;
-  
+
     if (grados >= 0 && grados < 30) {
       casillas = 3;
     } else if (grados >= 30 && grados < 90) {
@@ -47,54 +47,53 @@ const Roulette = ({ onClose }) => {
     } else if (grados >= 330 && grados < 360) {
       casillas = 3;
     }
-  
+
     setResultado(casillas);
-    onClose(casillas); // Llama a onClose con el número de casillas
-  }
+    onClose(casillas);
+  }, [rotation, onClose]); // Añadir 'onClose' como dependencia porque se está utilizando dentro del efecto.
 
   useEffect(() => {
     if (!isSpinning && showResult) {
-      final();
+      final(); // Usar la función memoizada
     }
-  }, [isSpinning, rotation, showResult, final]);
+  }, [isSpinning, showResult, final]);
 
   return (
     <div className='modal-overlay'>
       <div className='modal-content'>
-      <div className='plafon'>
-      <div className='close-button-container'> 
-        <button 
-          className='close-button' 
-          onClick={() => onClose(resultado)} // Asegúrate de que el botón de cerrar también llama a onClose con el resultado
-          disabled={isSpinning || !showResult}
-        >
-          <img src={closeImage} alt="Cerrar" />
-        </button>
-      </div>
-      <div className={`ruleta ${isSpinning ? 'spinning' : ''}`} style={{
-        backgroundImage: `url(${ruletaImage})`,
-        transform: `rotate(${rotation}deg)`,
-        transition: "transform 6s cubic-bezier(0.2,0.8,0.7,0.99)"
-      }}>
-      </div>
-      <div className='resultado-container' style={{ minHeight: '50px' }}>
-        {showResult ? (
-          <button className='resultado-button' onClick={() => onClose(resultado)}>
-          {resultado !== '' && `Avanza ${resultado} casillas`}
-        </button>
-        ) : (
-          <div className='barra-inferior'>
-            <button className='girar' onClick={lanzar} disabled={isSpinning}>
-              {isSpinning ? 'Girando...' : 'Girar'}
+        <div className='plafon'>
+          <div className='close-button-container'> 
+            <button 
+              className='close-button' 
+              onClick={() => onClose(resultado)} 
+              disabled={isSpinning || !showResult}
+            >
+              <img src={closeImage} alt="Cerrar" />
             </button>
           </div>
-        )}
-      </div>
-      <div className='flecha'>
-        <img src={flechaImage} alt='flecha'/>
-      </div>
-    </div>
-
+          <div className={`ruleta ${isSpinning ? 'spinning' : ''}`} style={{
+            backgroundImage: `url(${ruletaImage})`,
+            transform: `rotate(${rotation}deg)`,
+            transition: "transform 6s cubic-bezier(0.2,0.8,0.7,0.99)"
+          }}>
+          </div>
+          <div className='resultado-container' style={{ minHeight: '50px' }}>
+            {showResult ? (
+              <button className='resultado-button' onClick={() => onClose(resultado)}>
+              {resultado !== '' && `Avanza ${resultado} casillas`}
+            </button>
+            ) : (
+              <div className='barra-inferior'>
+                <button className='girar' onClick={lanzar} disabled={isSpinning}>
+                  {isSpinning ? 'Girando...' : 'Girar'}
+                </button>
+              </div>
+            )}
+          </div>
+          <div className='flecha'>
+            <img src={flechaImage} alt='flecha'/>
+          </div>
+        </div>
       </div>
     </div>
   );
