b11fd97d22ccc88f3d7b4e633945010d0cdd701e
Cristofer Paucar <151577243+Cristof10@users.noreply.github.com>
2024-08-15 00:47:01 -0500

    Preguntas aleatorias desde su propio componente

--- fisicaventura/src/components/SecondScreen.js
+++ fisicaventura/src/components/SecondScreen.js
@@ -1,32 +1,32 @@
 import React, { useState } from 'react';
-import SuggestionsForm from './SuggestionsForm';
-import ErrorReportForm from './ErrorReportForm';
-import logoImage from '../assets/images/logo.png';
 import SubHeader from './SubHeader';
 import Roulette from './Roulette';
 import Instructions from './Instructions';
-
+import Question from './Question';  // Importa el componente Question
 
 const SecondScreen = () => {
-
     const [showRoulette, setShowRoulette] = useState(true);
 
     const handleClose = () => {
         setShowRoulette(false);
         // Cuando se cierra la ruleta, se puede navegar aqui a otro componente
     };
+
     // Para que se muestre nuevamente la ruleta
     const showRouletteAgain = () => {
       setShowRoulette(true);
-  };
+    };
 
     return (
         <div>
             <SubHeader />
             {/* {showRoulette && <Roulette onClose={handleClose} />} */}
-                {/* Puedes agregar aquí otros componentes o lógica para mostrar 
-                cuando la ruleta esté cerrada */}
-            <Instructions/>
+            {/* Puedes agregar aquí otros componentes o lógica para mostrar 
+            cuando la ruleta esté cerrada */}
+            <Instructions />
+            
+            {/* Componente para mostrar la pregunta cargada */}
+            <Question />
         </div>
     );
 };
